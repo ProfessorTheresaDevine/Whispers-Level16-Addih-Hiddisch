d5f6fcf00fc3ac56d14bb04ea3c2b46961ac450b
EpikThePenguin <71152809+EpikThePenguin@users.noreply.github.com>
2020-09-26 21:03:28 -0700

    New pieces are pick-up-able
    The new pieces can be picked up without issue

--- Data/Scripts/PickUpPiece.lua
+++ Data/Scripts/PickUpPiece.lua
@@ -1,7 +1,9 @@
 ﻿-- player picks up an object and adds it to their inventory
+
 function handleOverlap(trigger, object)
+	print(trigger)
 	if object ~=nil and object:IsA("Player") then
-		object:AddResource("Bottle", 1)
+		object:AddResource("Piece", 1)
 		trigger.isEnabled = false
 	end
 end
